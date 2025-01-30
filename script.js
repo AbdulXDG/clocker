7046c2d3eda1cf7e75442478967b3f2ab8911457
abdulXDG <95185552+AbdulXDG@users.noreply.github.com>
2025-01-30 08:41:30 +0300

    Main

---
+++ script.js
@@ -0,0 +1,83 @@
+var intervalTimer;
+
+new Vue({
+  el: '#app',
+  data: {
+    selectedTime: 0,
+    timeLeft: '00:00',
+    endTime: '0',
+    times: [
+      {
+        sec: 30,
+        display: '30s'
+      },
+      {
+        sec: 60,
+        display: ' 1m'
+      },
+      {
+        sec: 600,
+        display: '10m'
+      },
+      {
+        sec: 900,
+        display: '15m'
+      }
+    ]
+  },
+  methods: {
+    setTime(seconds) {
+      clearInterval(intervalTimer);
+      this.timer(seconds);
+    },
+    timer(seconds) {
+      const now = Date.now();
+      const end = now + seconds * 1000;
+      this.displayTimeLeft(seconds);
+
+      this.selectedTime = seconds;
+      // this.initialTime = seconds;
+      this.displayEndTime(end);
+      this.countdown(end);
+    },
+    countdown(end) {
+      // this.initialTime = this.selectedTime;
+      intervalTimer = setInterval(() => {
+        const secondsLeft = Math.round((end - Date.now()) / 1000);
+
+        if(secondsLeft === 0) {
+          this.endTime = 0;
+        }
+
+        if(secondsLeft < 0) {
+          clearInterval(intervalTimer);
+          return;
+        }
+        this.displayTimeLeft(secondsLeft)
+      }, 1000);
+    },
+    displayTimeLeft(secondsLeft) {
+      const minutes = Math.floor((secondsLeft % 3600) / 60);
+      const seconds = secondsLeft % 60;
+
+      this.timeLeft = `${zeroPadded(minutes)}:${zeroPadded(seconds)}`;
+    },
+    displayEndTime(timestamp) {
+      const end = new Date(timestamp);
+      const hour = end.getHours();
+      const minutes = end.getMinutes();
+
+      this.endTime = `${hourConvert(hour)}:${zeroPadded(minutes)}`
+    },
+  }
+})
+
+function zeroPadded(num) {
+  // 4 --> 04
+  return num < 10 ? `0${num}` : num;
+}
+
+function hourConvert(hour) {
+  // 15 --> 3
+  return (hour % 12) || 12;
+}
